5e479c5dc190af6713f7dc90049ba771717a4a39
dermawanbagus356-debug <dermawanbagus356@gmail.com>
2026-06-22 21:44:23 +0700

    "Implementasi Custom Exception dan Handling"

--- Pertemuan_8/exception_handling_lab.py
+++ Pertemuan_8/exception_handling_lab.py
@@ -1,1 +1,35 @@
+# Custom Exception
+class NilaiTidakValidError(Exception):
+    pass
 
+
+# Class Utama
+class Mahasiswa:
+    def __init__(self, nama):
+        self.nama = nama
+
+    def input_nilai(self, nilai):
+        if nilai < 0 or nilai > 100:
+            raise NilaiTidakValidError(
+                "Nilai harus berada di antara 0 sampai 100!"
+            )
+
+        print(f"Nilai {nilai} berhasil disimpan.")
+
+
+# Program Utama
+try:
+    mhs = Mahasiswa("Bagus")
+
+    nilai = int(input("Masukkan nilai mahasiswa: "))
+
+    mhs.input_nilai(nilai)
+
+except NilaiTidakValidError as e:
+    print("Error:", e)
+
+except ValueError:
+    print("Input harus berupa angka!")
+
+finally:
+    print("Proses pemeriksaan nilai telah selesai dilakukan.")
